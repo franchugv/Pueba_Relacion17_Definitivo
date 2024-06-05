7328cd5bde4ac237e27ab8e5ce43cf84cacccdea
Francisco Baena <insfrana@outlook.com>
2024-06-05 10:10:11 +0200

    VB BV

--- Pueba_Relacion17/Clases/Producto.cs
+++ Pueba_Relacion17/Clases/Producto.cs
@@ -103,5 +103,7 @@ namespace ClaseProducto
             return precioInc;
         }
 
+
+        // MÉTODOS
     }
 }
--- Pueba_Relacion17/Test/TestR17.cs
+++ Pueba_Relacion17/Test/TestR17.cs
@@ -15,7 +15,6 @@ namespace Pueba_Relacion17
 
             // Validar que los datos no sean vacíos, debe ser "desconocidos"
             Assert.IsNotNull(producto);
-
             // Sí no es desconocido, el Asser dará error, el precio IVA está inicializado a -1
             if (producto.Nombre != "Desconocido" | producto.Precio != 0 | producto.PrecioIVA != -1) Assert.Fail();
 
@@ -37,7 +36,6 @@ namespace Pueba_Relacion17
         public void ValidarConstructorNombre()
         {
             Producto productoNombre = new Producto("Fran");
-
             // Validar que el precio no sea nulo
             Assert.IsNotNull(productoNombre.Precio);
         }
@@ -45,14 +43,23 @@ namespace Pueba_Relacion17
         [Test]
         public void ValidarConstructorCompleto()
         {
-            Producto producto = new Producto("Fran", 40);
-
+            Producto producto = new Producto("Fran", 20);
             // Validar que se calcule el precio IVA
             Assert.AreNotEqual(producto.Precio, producto.PrecioIVA);
-
-            // Validar Precio IVA y verificar que la propiedad funcione correctamente
-            producto.Precio = 20;
+            // Validar Precio IVA
             Assert.AreEqual(producto.PrecioIVA, 24.2f);
+
+        }
+
+        [Test]
+        public void ValidarPrecio()
+        {
+            // La precio del producto debe estar entre 50 y 350 € (50 y 350 incluidos)
+            Producto producto = new Producto("Fran", 50);
+            // Vamos a validar que el precio no sea mayor que 350
+            Assert.LessOrEqual(producto.Precio, 350);
+            // Vamos a validar que el precio no sea menor a 50
+            Assert.GreaterOrEqual(producto.Precio, 50);
 
         }
     }
